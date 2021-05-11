17670db474913f5300126b3f86035964915a9f62
George McEntegart <geerodge@gmail.com>
2021-05-12 00:46:43 +0100

    SingleTea.js state tracking and functionality added with some css

---
+++ src/styles/SingleTeaStyles.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+const SingleTeaStyles = styled.div`
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas:
+        "product";
+
+    .product-options {
+        margin-bottom: 20px;
+
+        select, .addcart {
+            width: 100%;
+            margin: 20px 0px;
+            padding: 15px;
+        }
+        select {
+            background-color: white;
+            border: 1px solid #e1e2e2;
+        }
+        .addcart:disabled{
+            background:  #fff;
+            color: #ccc;
+            cursor: no-drop;
+            border: 1px solid #e1e2e2;
+        }
+    }
+
+    .quantity {
+        display: grid;
+        grid-template-columns: 25% 50% 25%;
+        grid-template-rows: 1fr;
+
+        border: 1px solid #e1e2e2;
+
+        button {
+            padding: 15px 0 15px 0;
+            background-color: var(--white);
+            color: var(--black);
+            font-weight: bold;
+            font-size: 28px;
+        }
+        button:first-child {
+            border-radius: 4px 0px 0px 4px;
+            border-right: 1px solid #e1e2e2;
+        }
+        button:last-child {
+            border-radius: 0px 4px 4px 0px;
+            border-left: 1px solid #e1e2e2;
+        }
+        input {
+            text-align: center;
+            border: none;
+        }
+        input:focus {
+            margin: 0 5px 0 5px;
+        }
+    }
+
+    /* Hides the silly default arrows/spinners from input type number */
+    /* Chrome, Safari, Edge, Opera */
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button { 
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+`;
+
+export default SingleTeaStyles;
--- src/templates/SingleTea.js
+++ src/templates/SingleTea.js
@@ -1,67 +1,7 @@
 import React, { useState } from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
-import styled from "styled-components";
-
-const ProductStyles = styled.div`
-
-    @media only screen and (min-width: 900px) {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-rows: 1fr 1fr;
-        gap: 4rem;
-        grid-template-areas:
-            "product-picture product-detail"
-            "product-info product-info";
-
-        margin-top: 50px;
-        .product-picture {
-            grid-area: product-picture;
-        }
-        .product-detail {
-            grid-area: product-detail;
-            .vat {
-                font-size: 3rem;
-            }
-        }
-        .product-info {
-            grid-area: product-info;
-        }
-
-        .options {
-            margin: 5px;
-            max-width: 250px;
-            min-width: 250px;
-            input {
-                max-width: 100px;
-            }
-            input[type=number]::-webkit-inner-spin-button, 
-            input[type=number]::-webkit-outer-spin-button { 
-                -webkit-appearance: none; 
-                margin: 0; 
-            }
-            input[type=number] {
-                width: 45px;
-                height: 36px;
-                text-align: center;
-            }
-        }
-        .quantity {
-            margin: 0px;
-            button {
-                padding: 1rem 2rem;
-                margin: 5px;
-            }
-        }
-        .sizes {
-            height: 36px;
-        }
-        .addcart {
-            height: 36px;
-        }
-    }
-
-`;
+import ProductStyles from "../styles/SingleTeaStyles";
 
 // Product data is passed in via context in gatsby-node.js
 export default function SingleProductPage({ pageContext: { page }, data: { allSanityTea } }) {
@@ -89,36 +29,59 @@ function decrease() {
         setCount(count - 1);
     }
 }
+
+//Track state of select element for product sizes and disable add to basket button if default value selected
+const [selectedValue, setValue] = useState("Select Size");
+
     return (
     <ProductStyles>
-        <div className="product-picture">
-            <Img fluid={teaProduct.image.asset.fluid} alt={teaProduct.name} />
-        </div>
-        <div className="product-detail">
+        <div className="product">
             <h1>{teaProduct.name}</h1>
             <p><span className="vat">from {formatMoney(teaProduct.price / 100)}</span> inc VAT</p>
-            <NewlineText text={teaProduct.description} />
-            <select className="sizes options">
-                <option disabled hidden selected>Select Size</option>
-                {teaProduct.tea_weight.map((productSize,i) => (
-                    <option key={i} value={productSize.weight}>{productSize.name}</option>))}
-            </select>
-            <div className="quantity options">
-                <button type="button" onClick={decrease}>-</button>
-                <input type="number" name="quantity" value={count} />
-                <button type="button" onClick={increase}>+</button>
+            <Img fluid={teaProduct.image.asset.fluid} alt={teaProduct.name} />
+            <div className="product-options">
+                <select
+                    onChange={(e) => setValue(e.target.value)}
+                    name="sizes"
+                    defaultValue={selectedValue}
+                >
+                    <option disabled hidden value="Select Size">Select Size</option>
+                        {teaProduct.tea_weight.map((productSize,i) => (
+                            <option 
+                                key={i}
+                                value={productSize.weight}>{productSize.name}
+                            </option>
+                        ))}
+                </select>
+                <div className="quantity">
+                    <button type="button" onClick={decrease}>-</button>
+                    <input type="number" name="quantity" value={count} />
+                    <button type="button" onClick={increase}>+</button>
+                </div>
+                <button 
+                    type="button" 
+                    className="addcart" 
+                    disabled={selectedValue === "Select Size" ? true : null}
+                >
+                    {selectedValue === "Select Size" ? "Please select size" : "Add to basket"}
+                </button>
             </div>
-            <button type="button" className="addcart options">Add to basket</button>
-        </div>
-        <div className="product-info">
+            <h2>Description</h2>
+            <NewlineText 
+                text={teaProduct.description}
+            />
             <h2>Did You Know...</h2>
-            <NewlineText text={teaProduct.did_you_know} />
+            <NewlineText 
+                text={teaProduct.did_you_know}
+            />
             <h2>Brewing Instructions</h2>
             <p>{teaProduct.brewing_instructions}</p>
             <h2>Ingredients</h2>
             <p>{teaProduct.ingredients}</p>
             <h2>Allergy Information</h2>
-            <p>{teaProduct.allergy ? `Dairy free, gluten free, suitable for vegetarians and vegans. Packed in a factory which handles nuts.` : ``}</p>
+            <p>
+                {teaProduct.allergy ? `Dairy free, gluten free, suitable for vegetarians and vegans. Packed in a factory which handles nuts.` : ``}
+            </p>
         </div>
     </ProductStyles>
     )
