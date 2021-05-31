6fc26408a42ed87e838e37860f8bf46b2d728f15
George McEntegart <geerodge@gmail.com>
2021-05-31 17:28:32 +0100

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -26,28 +26,31 @@ const ContactFormStyles = styled.div`
 `;
 
 export default function ContactForm() {
-  <ContactFormStyles>
-    <form name="contact" method="POST" data-netlify="true">
-        <div>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" required />
-        </div>
-        <div>
-            <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-            <label htmlFor="tel">Telephone</label>
-            <input type="tel" id="tel" name="tel" required />
-        </div>
-        <div>
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" required />
-        </div>
-        <div>
-            <div data-netlify-recaptcha="true"></div>
-        </div>
-        <button type="submit">Send Message</button>
-    </form>
-  </ContactFormStyles>
+  return (
+    <ContactFormStyles>
+      <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="contact" value="contact" />
+          <div>
+              <label htmlFor="name">Your Name</label>
+              <input type="text" id="name" name="name" required />
+          </div>
+          <div>
+              <label htmlFor="email">Email Address</label>
+              <input type="email" id="email" name="email" required />
+          </div>
+          <div>
+              <label htmlFor="tel">Telephone</label>
+              <input type="tel" id="tel" name="tel" required />
+          </div>
+          <div>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message" required />
+          </div>
+          <div>
+              <div data-netlify-recaptcha="true"></div>
+          </div>
+          <button type="submit">Send Message</button>
+      </form>
+    </ContactFormStyles>
+  )
 };
